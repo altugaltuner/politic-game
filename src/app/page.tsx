3ef251b02bfc912d6b2c5c1e72ac446578ec8ace
Altuğ Altuner <vefgend@gmail.com>
2024-11-19 13:22:31 +0300

    anasayfa değişti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,36 +26,9 @@ export default function HomePage() {
         </p>
       </div>
 
-      <Header />
-      <div className="h-auto md:h-[90%] lg:w-[90%] w-full flex flex-col md:gap-3 gap-2 justify-around items-center bg-white sm:p-5 p-2 rounded-3xl border-[5px] border-black">
-        <h2 className="font-bold xl:text-[45px] lg:text-[32px] text-[30px]">Nasıl Oynanır</h2>
-        <div className="md:flex gap-2 grid sm:grid-cols-2 grid-cols-1 w-full">
-          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
-            <h3 className="lg:text-lg text-base font-semibold text-center">Başlangıç ve Hedefler</h3>
-            <Image src={president} alt={"president"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
-            <p className="lg:text-sm text-xs">Cumhurbaşkanı olarak başlıyorsun. Amacın bir ay boyunca ülkeyi yönetebilmek. Her soru ise bir güne eşit oluyor.</p>
-          </div>
-          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
-            <h3 className="lg:text-lg text-base font-semibold text-center">Karar Verme Süreci</h3>
-            <Image src={choices} alt={"choices"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
-            <p className="lg:text-sm text-xs">Soruların iki cevabı ve her cevabın farklı etkileri var. Bu cevaplar halk desteği, bütçe, politika gibi kaynakları etkiliyor.</p>
-          </div>
-          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
-            <h3 className="lg:text-lg text-base font-semibold text-center">Kaynak Yönetimi</h3>
-            <Image src={stats} alt={"stats"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
-            <p className="lg:text-sm text-xs"> Kaynakları en az 30 oyun günü boyunca sıfırın üstünde tutmaya çalışmalısın. Ayrıca bazı iraden dışında gelişen olaylar da kaynakları doğrudan etkiliyor.</p>
-          </div>
-          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
-            <h3 className="lg:text-lg text-base font-semibold text-center">Oyunun Sonu</h3>
-            <Image src={skulls} alt={"skulls"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
-            <p className="lg:text-sm text-xs">Eğer bir kaynağın değeri sıfırın altına düşerse iktidarın sona eriyor. Ancak 30 gün boyunca iktidarda kalırsan görevini başarıyla tamamlıyorsun</p>
-          </div>
-        </div>
-      </div>
 
-      <div className="h-auto md:h-[90%] lg:w-[90%] w-full flex flex-col md:gap-3 gap-2 justify-around items-center bg-white sm:p-3 p-1 rounded-3xl border-[5px] border-black sm:mb-[80px] mb-[80px]">
+      <div className="h-auto md:h-[90%] lg:w-[90%] w-full flex flex-col md:gap-3 gap-2 justify-around items-center bg-white sm:p-3 p-1 rounded-3xl border-[5px] border-black">
         <h2 className="font-bold xl:text-[45px] lg:text-[32px] text-[30px]">Hemen Başla</h2>
-
         <form className="flex gap-3 sm:flex-row flex-col items-center pb-3" onSubmit={(e) => {
           e.preventDefault();
         }}>
@@ -87,8 +60,35 @@ export default function HomePage() {
             İletişim
           </Button>
         </form>
-
       </div>
+
+      <Header />
+      <div className="h-auto md:h-[90%] lg:w-[90%] w-full flex flex-col md:gap-3 gap-2 justify-around items-center bg-white sm:p-5 p-2 rounded-3xl border-[5px] border-black mb-[80px]">
+        <h2 className="font-bold xl:text-[45px] lg:text-[32px] text-[30px]">Nasıl Oynanır</h2>
+        <div className="md:flex gap-2 grid sm:grid-cols-2 grid-cols-1 w-full">
+          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
+            <h3 className="lg:text-lg text-base font-semibold text-center">Başlangıç ve Hedefler</h3>
+            <Image src={president} alt={"president"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
+            <p className="lg:text-sm text-xs">Cumhurbaşkanı olarak başlıyorsun. Amacın bir ay boyunca ülkeyi yönetebilmek. Her soru ise bir güne eşit oluyor.</p>
+          </div>
+          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
+            <h3 className="lg:text-lg text-base font-semibold text-center">Karar Verme Süreci</h3>
+            <Image src={choices} alt={"choices"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
+            <p className="lg:text-sm text-xs">Soruların iki cevabı ve her cevabın farklı etkileri var. Bu cevaplar halk desteği, bütçe, politika gibi kaynakları etkiliyor.</p>
+          </div>
+          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
+            <h3 className="lg:text-lg text-base font-semibold text-center">Kaynak Yönetimi</h3>
+            <Image src={stats} alt={"stats"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
+            <p className="lg:text-sm text-xs"> Kaynakları en az 30 oyun günü boyunca sıfırın üstünde tutmaya çalışmalısın. Ayrıca bazı iraden dışında gelişen olaylar da kaynakları doğrudan etkiliyor.</p>
+          </div>
+          <div className="md:w-[24%] w-full flex flex-col gap-2 justify-start items-center text-start border-2 border-black p-2 rounded-xl">
+            <h3 className="lg:text-lg text-base font-semibold text-center">Oyunun Sonu</h3>
+            <Image src={skulls} alt={"skulls"} width={200} height={200} className="md:w-auto w-[90%] border-[2px] rounded-md border-black" />
+            <p className="lg:text-sm text-xs">Eğer bir kaynağın değeri sıfırın altına düşerse iktidarın sona eriyor. Ancak 30 gün boyunca iktidarda kalırsan görevini başarıyla tamamlıyorsun</p>
+          </div>
+        </div>
+      </div>
+
       <footer className="absolute bottom-0 w-full bg-black text-white text-center sm:p-2 p-1 text-xs sm:text-sm">
         <p>© 2024 Cumhurbaşkanı Simulator. Tüm hakları saklıdır.</p>
         <div className="flex justify-center sm:gap-5 gap-2 mt-2">
