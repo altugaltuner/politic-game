95f7c82eb82a497330c3970c9786c07dd6172bea
Altuğ Altuner <vefgend@gmail.com>
2024-12-06 20:04:28 +0300

    m

--- src/firebase.js
+++ src/firebase.js
@@ -17,6 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+
 // Initialize services
 export const db = getFirestore(app); // Firestore
 export const auth = getAuth(app); // Authentication
